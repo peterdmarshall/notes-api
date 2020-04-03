167199a2e448b986fc32290d375067a539ae9887
Peter Marshall <peterdmarshall99@gmail.com>
2020-04-03 10:30:50 -0400

    Docker runs in detached mode in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ssh -T root@petermarshall.dev <<EOI
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker pull ${DOCKER_USERNAME}/notes-api:latest
-docker run -p 3000:3000 -e MONGO_URI='${MONGO_URI}' ${DOCKER_USERNAME}/notes-api
+docker run -d -p 3000:3000 -e MONGO_URI='${MONGO_URI}' ${DOCKER_USERNAME}/notes-api
 
 exit 
 EOI
