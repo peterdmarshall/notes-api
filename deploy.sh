4fef78f28458baef7325fb9f5aa80a07de1fce4f
Peter Marshall <peterdmarshall99@gmail.com>
2020-04-03 10:15:32 -0400

    Edit deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ssh -T root@petermarshall.dev <<EOI
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker pull ${DOCKER_USERNAME}/notes-api:latest
-docker run -p 3000:3000 -env MONGO_URI=${MONGO_URI} ${DOCKER_USERNAME}/notes-api:latest
+docker run -p 3000:3000 -env MONGO_URI=${MONGO_URI} ${DOCKER_USERNAME}/notes-api
 
 exit 
 EOI
